5ae20c2579def2993e548b998ad68ffa070e5302
yona muller <rsymuller@gmail.com>
2024-05-29 19:55:10 +0300

    start build the building class

--- class_building.py
+++ class_building.py
@@ -1,3 +1,19 @@
-class building:
-    def __init__(self) -> None:
-        pass
+import pygame
+from class_floor import Floor
+from class_elevator import Elevator
+
+class Building:
+    def __init__(self, num_floors, num_elevators) -> None:
+        self.__floors = [Floor(i) for i in range(num_floors + 1)]
+        self.__elevators = [Elevator(i) for i in range(num_elevators)]
+        print(self.__floors)
+
+    def build_floors(self, screen, height):
+        for floor in self.__floors:
+            floor.image_floor(screen, height - 100)
+            height -= 87  
+
+    def build_elevators(self, screen, height, a = 330):
+        for elevator in self.__elevators:
+            elevator.image_elevator(screen, a, height - 100)
+            a += 75
